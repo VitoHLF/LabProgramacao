8b209f2ce0f90f7e821b816487ed84af0cdf78b2
Victor Hugo Ferreira <victor.hugo.lopes.ferreira@gmail.com>
2023-10-11 20:12:25 -0300

    Sensor de distancia ultrassom

--- 231011_SensorDistancia/sketch_oct11a/sketch_oct11a.ino
+++ 231011_SensorDistancia/sketch_oct11a/sketch_oct11a.ino
@@ -2,13 +2,41 @@
 Emite uma rajada de pulsos em ultrassom e recebe o eco de obstáculos à frente.
 Pelo tempo entre a rajada e o eco é possível calcular a distância do obstáculo.
 Pode medir distâncias de 3cm até 3m.
- */
-void setup() {
-  // put your setup code here, to run once:
+Pinagem:
+  -Vcc
+  -Trigger: Pino de saída (digital)
+  -Echo: Pino de entrada (digital)
+  -Gnd
 
+  Disparo:
+digitalWrite(pinTrig, LOW);
+delayMicroseconds(2);
+digitalWrite(pinTrig, HIGH);
+delayMicroseconds(10);
+unsigned long tempo = pulsein(pinEcho, HIGH);
+int cm = tempo*1717/100000;
+ */
+#define pinTrig 7
+#define pinEcho 8
+
+unsigned long tempo;
+int distmm;
+
+void setup() {
+  Serial.begin(57600);
+  pinMode(pinTrig, OUTPUT);
+  pinMode(pinEcho, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  digitalWrite(pinTrig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pinTrig, HIGH);
+  delayMicroseconds(10);
+  tempo = pulseIn(pinEcho, HIGH);
+  distmm = tempo*1717/10000;
 
+  Serial.println(distmm);
+  delay(200);
 }
